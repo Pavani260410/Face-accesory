16d102e6fc1d24788734c946538a169308870d71
Pavani260410 <69670894+Pavani260410@users.noreply.github.com>
2021-08-14 10:49:34 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -10,34 +10,29 @@ var maskw = 350;
 var maskh = 350;
 function add() {
   face = new Image();
-  face.onload = uploadFace;
   face.src = myface;
-
   mask = new Image();
-  mask.onload = uploadMask;
+  mask.onload = uploadMaskandface;
   mask.src = mymask;
 }
-function uploadFace() {
-    ctx.drawImage(face, 0, 0, 600, 450);
-}
-function uploadMask() {
+
+function uploadMaskandface() {
+     ctx.clearRect(0, 0, ctx.canvas.width, ctx.canvas.height);
+     ctx.drawImage(face, 0, 0, 600, 450);
      ctx.drawImage(mask, maskx, masky, maskw, maskh);
     }
 function xchange() {
      maskx = document.getElementById('x').value;
-     uploadFace();
-     uploadMask();
+     uploadMaskandface();
     }
 function ychange() {
         masky = document.getElementById('y').value;
-        uploadFace();
-        uploadMask();
+        uploadMaskandface();
        }
 
 function wchange() {
         maskw = document.getElementById('w').value;
-        uploadFace();
-        uploadMask();
+        uploadMaskandface();
        }
 function hchange() {
            maskh = document.getElementById('h').value;
@@ -48,12 +43,13 @@ function changemask() {
             mymask = document.getElementById('mask').value;
             console.log(mymask);
            add();
+           document.getElementById('mask').value = "";
            }
 function changeface() {
             myface = document.getElementById('face').value;
             console.log(myface);
            add();
-           uploadFace();
-        uploadMask();
+           document.getElementById('face').value = "";
            }
+          
  
